41f7f594f7dca250a097224d63ae520f439ddb92
zhuanyeshiti <liuhongwei123456@gmail.com>
2016-12-24 22:24:00 +0800

    add pm.inc

--- hello.asm
+++ hello.asm
@@ -1,18 +1,24 @@
-org 07c00h
-mov ax,cs
-mov ds,ax
-mov es,ax
-call dispstr
-jmp $
-dispstr:
-mov ax,bootmessage
-mov bp,ax
-mov cx,16
-mov ax,01301h
-mov bx,000ch
-mov dl,0
-int 10h
-ret
-bootmessage: db "hello,world!"
-times 510-($-$$) db 0
-dw 0xaa55
+#include "pm.inc"
+
+	org 07c00h
+		jmp label_begin
+	[section .gdt]
+	
+label_begin:
+	mov ax,cs
+	mov ds,ax
+	mov es,ax
+	call dispstr
+	jmp $
+	dispstr:
+	mov ax,bootmessage
+	mov bp,ax
+	mov cx,16
+	mov ax,01301h
+	mov bx,000ch
+	mov dl,0
+	int 10h
+	ret
+	bootmessage: db "hello,world!"
+	times 510-($-$$) db 0
+	dw 0xaa55
