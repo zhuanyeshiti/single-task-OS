820c28adebaf2ed301acb95153f9875a3e3ea689
zhuanyeshiti <liuhongwei123456@gmail.com>
2016-12-24 21:39:23 +0800

    add and modify

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 hello.bin : hello.asm
 	nasm -o hello.bin hello.asm
+	dd if=hello.bin of=hello.img count=512 bs=1
+clean:
+	rm -rf *.bin *.img
--- hello.asm
+++ hello.asm
@@ -1,1 +1,18 @@
-org 0100h
+org 07c00h
+mov ax,cs
+mov ds,ax
+mov es,ax
+call dispstr
+jmp $
+dispstr:
+mov ax,bootmessage
+mov bp,ax
+mov cx,16
+mov ax,01301h
+mov bx,000ch
+mov dl,0
+int 10h
+ret
+bootmessage: db "hello,world!"
+times 510-($-$$) db 0
+dw 0xaa55
