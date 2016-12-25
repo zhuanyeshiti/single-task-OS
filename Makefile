513382877f06bc694f633d09719f156ac3f825c9
zhuanyeshiti <liuhongwei123456@gmail.com>
2016-12-25 22:35:37 +0800

    add and modify files

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-hello.bin : hello.asm
+hello.bin : hello.asm pm.inc
 	nasm -o hello.bin hello.asm
 	dd if=hello.bin of=hello.img count=512 bs=1
 clean:
--- hello.asm
+++ hello.asm
@@ -1,13 +1,52 @@
-#include "pm.inc"
+%include "pm.inc"
 
 	org 07c00h
-		jmp label_begin
-	[section .gdt]
-	
+	jmp label_begin
+[section .gdt]
+label_gdt:	descriptor	0,	0,	0
+label_desc_code32: descriptor	0,	segcode32len-1,	da_c+da_32
+label_desc_video: descriptor	0b8000h,	0ffffh,	da_drw
+	gdtlen	equ	$-label_gdt
+	gdtptr	dw	gdtlen-1
+		dd	0
+	selectorcode32	equ	label_desc_code32-label_gdt
+	selectorvideo	equ	label_desc_video-label_gdt
+[section .s16]
+[bits 16]
 label_begin:
 	mov ax,cs
 	mov ds,ax
 	mov es,ax
+	mov ss,ax
+	mov sp,0100h
+	xor eax,eax
+	mov ax,cs
+	shl eax,4
+	add eax,label_seg_code32
+	mov word [label_desc_code32+2],ax
+	shr eax,16
+	mov byte [label_desc_code32+4],al
+	mov byte [label_desc_code32+7],ah
+
+	xor eax,eax
+	mov ax,ds
+	shl eax,4
+	add eax,label_gdt
+	mov dword [gdtptr+2],eax
+	lgdt [gdtptr]
+
+	cli
+
+	in al,92h
+	or al,00000010b
+	out 92h,al
+
+	mov eax,cr0
+	or eax,1
+	mov cr0,eax
+
+	jmp dword selectorcode32:0
+
 	call dispstr
 	jmp $
 	dispstr:
@@ -20,5 +59,17 @@ label_begin:
 	int 10h
 	ret
 	bootmessage: db "hello,world!"
+
+label_seg_code32:
+	mov ax,selectorvideo
+	mov gs,ax
+	mov edi,(80*11+79)*2
+	mov ah,0ch
+	mov al,'P'
+	mov [gs:edi],ax
+	jmp $
+
+	segcode32len	equ	$-label_seg_code32
+
 	times 510-($-$$) db 0
 	dw 0xaa55
